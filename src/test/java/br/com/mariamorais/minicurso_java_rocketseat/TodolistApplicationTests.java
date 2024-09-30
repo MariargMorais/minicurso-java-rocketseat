f72358bcc8f75a8a18a0221119c4e91f436dbcb6
Maria Morais <maria.morais@liferay.com>
2024-09-30 16:47:20 -0300

    Aula 03

---
+++ src/test/java/br/com/mariamorais/minicurso_java_rocketseat/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mariamorais.minicurso_java_rocketseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
